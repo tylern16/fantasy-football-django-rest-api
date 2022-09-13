4eca4925575cfbd73dc690d0c155ab2106e854d6
tylern16 <ty.nenninger@gmail.com>
2022-09-13 14:56:33 -0500

    initialize

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # add this
+DISABLE_COLLECTSTATIC = 1
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
