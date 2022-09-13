b361de3e8082062f084c10c37fbab5acf164e134
tylern16 <ty.nenninger@gmail.com>
2022-09-13 14:46:22 -0500

    initialize

--- nflproject_api/urls.py
+++ nflproject_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/api/team', views.TeamList.as_view(), name='team_list'), # api/team will be routed to the teamList view for handling
+    path('/teams', views.TeamList.as_view(), name='team_list'), # api/team will be routed to the teamList view for handling
     path('/api/team/<int:pk>', views.TeamDetail.as_view(), name='team_detail'), # api/team will be routed to the teamDetail view for handling
 ]
